83388bcc03bb1a766db5b945c0a4b7a24fd519ba
merdan1244 <merdan-kaplan@hotmail.com>
2023-03-19 18:02:03 +0000

    first_practice

---
+++ first_script.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "this script name is $0" ## $0 represents script itself by presenting the name
+
+echo "Hello there $1 $2 $3 $4" ## any shell script by default has 9 arguments {1..9}
+
+echo "THis argument is the list of all arguments given by the user: $0"
+
+echo "This scriot is running with this PID: $$"
+
+### git pull
+### git add.
+### git commit -m "some comments"
+### git push 
+
+
